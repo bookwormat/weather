34cb27dc800f8151a833aa715d848bed0d4caadf
Michael Groh <michael@mgroh.net>
2018-02-06 14:10:46 +0100

    Dispatch CLEAR_WEATHER action in epic

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,10 +1,12 @@
 import {Observable} from 'rxjs';
 import {combineEpics} from 'redux-observable'
+import {clear, LOAD_WEATHER} from "../actions";
 
 const loadWeatherEpic = (action$) => {
-  return action$
-    .do(action => console.log(action))
-    .ignoreElements();
+  return action$.ofType(LOAD_WEATHER)
+    .switchMap(() => {
+      return Observable.of(clear()).delay(2000);
+    })
 };
 
 export const rootEpic = combineEpics(loadWeatherEpic);
