b42b85d16977072dab2c5738cdc805eec5e9ab4f
Michael Groh <michael@mgroh.net>
2018-02-15 09:06:41 +0100

    Use fela to style a component to try it out

--- src/components/ActiveLocation.js
+++ src/components/ActiveLocation.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { createComponent } from 'react-fela'
+
+const Container = createComponent(({ padding }) => ({
+  padding: padding + 'px',
+  backgroundColor: 'rgb(124, 114, 231)',
+  fontSize: '20px'
+}));
 
 const ActiveLocation = ({activeLocation}) =>
-  <div>
+  <Container padding={20}>
     {activeLocation && <h1>Weather in {activeLocation.name}</h1>}
-  </div>;
+  </Container>;
 
 export default ActiveLocation;
